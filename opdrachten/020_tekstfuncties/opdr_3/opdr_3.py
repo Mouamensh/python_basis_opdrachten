b509ee1ad9e13b636afd2b6f04a25b317563db47
Your Name <you@example.com>
2023-10-05 11:46:30 +0200

    pdr_3

--- opdrachten/020_tekstfuncties/opdr_3/opdr_3.py
+++ opdrachten/020_tekstfuncties/opdr_3/opdr_3.py
@@ -1,5 +1,8 @@
-# Opdracht 3 tekstfuncties
-# Naam student:
-# Groep:
-
-# Hier komt je code...
+print("    *              " * 5)
+print("   ***             " * 5)
+print("  ******           " * 5)
+print(" ********          " * 5)
+print("***********        " * 5)
+print("    ***            " * 5)
+print("    ***            " * 5)
+print("    ***            " * 5)
