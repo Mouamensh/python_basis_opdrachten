71f4c285e66ab1616069d99b77652ec8d411508e
Your Name <you@example.com>
2023-10-05 11:54:23 +0200

    opdr_1

--- opdrachten/030_berekeningen/opdr_1/opdr_1.py
+++ opdrachten/030_berekeningen/opdr_1/opdr_1.py
@@ -3,3 +3,16 @@
 # Groep:
 
 # Hier komt je code...
+joe = 23
+bob = 15
+fred = 22
+# Bereken de totale leeftijd
+totale_leeftijd = joe + bob + fred
+
+# Bereken de gemiddelde leeftijd
+gemiddelde_leeftijd = totale_leeftijd / 3
+
+# Geef de resultaten weer op het scherm
+print("Dit weten we over joe, bob en fred:")
+print("De totale waarde van alle leeftijden bij elkaar opgeteld =", totale_leeftijd)
+print("De gemiddelde leeftijd van deze boys is:", gemiddelde_leeftijd)
