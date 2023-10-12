3b27310e5b6b8da276423e21720786133ec7c880
Your Name <you@example.com>
2023-10-12 12:19:24 +0200

    opdracht5-3

--- opdrachten/050_input_function/opdr_3/opdr_3.py
+++ opdrachten/050_input_function/opdr_3/opdr_3.py
@@ -4,3 +4,23 @@
 
 # Hier komt je code...
 
+# Vraag de gebruiker om een lijst met steden in te voeren
+input_string = input("Voer minimaal 5 steden in, gescheiden door komma's: ")
+
+# Splits de invoer op basis van komma's en verwijder eventuele extra spaties
+steden = [stad.strip() for stad in input_string.split(',')]
+
+# Filter de steden die met "Z" beginnen
+z_steden = [stad for stad in steden if stad.startswith("Z")]
+
+# Sorteer de lijst in omgekeerde volgorde
+z_steden.sort(reverse=True)
+
+# Verwijder de steden die met "Z" beginnen uit de oorspronkelijke lijst
+steden = [stad for stad in steden if not stad.startswith("Z")]
+
+# Voeg de gesorteerde "Z"-steden weer vooraan toe aan de lijst
+steden = z_steden + steden
+
+# Druk de gesorteerde lijst af
+print(steden)
