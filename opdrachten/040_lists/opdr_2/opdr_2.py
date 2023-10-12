b6ccd643724ea699c716efa193ab21ef2baa388c
Your Name <you@example.com>
2023-10-12 11:54:12 +0200

    oopdracht4-2

--- opdrachten/040_lists/opdr_2/opdr_2.py
+++ opdrachten/040_lists/opdr_2/opdr_2.py
@@ -13,3 +13,19 @@ rivieren = list(rivier_info.keys())
 # rivieren is nu een list met alleen de riviernamen: ['rijn', 'maas', 'nijl']
 
 # Hier jouw code.....
+rivier_info = { 
+    "rijn": ["nederland", "duitsland", "Frankrijk"], 
+    "maas": ["nederland", "belgië", "duitsland"], 
+    "nijl": ["egypte", "soedan", "oeganda"] 
+}
+
+rivieren = list(rivier_info.keys())
+
+# Afdrukken van de naam van de 1e rivier en het 2e land waar de 1e rivier doorheen stroomt
+print(f"De rivier {rivieren[0].capitalize()} stroomt onder andere door {rivier_info[rivieren[0]][1].capitalize()}")
+
+# Afdrukken van de naam van de 2e rivier en het 1e land waar de 2e rivier doorheen stroomt
+print(f"De rivier {rivieren[1].capitalize()} stroomt onder andere door {rivier_info[rivieren[1]][0].capitalize()}")
+
+# Afdrukken van de naam van de 3e rivier en het 3e land waar de 1e rivier doorheen stroomt
+print(f"De rivier {rivieren[2].capitalize()} stroomt onder andere door {rivier_info[rivieren[0]][2].capitalize()}")
