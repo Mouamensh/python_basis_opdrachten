9c1b63f417b23b724c7623d941f1769ad45b3018
Your Name <you@example.com>
2023-10-12 11:46:35 +0200

    opdracht1

--- opdrachten/040_lists/opdr_1/opdr_1.py
+++ opdrachten/040_lists/opdr_1/opdr_1.py
@@ -2,8 +2,17 @@
 # Naam student:
 # Groep:
 
-mylist = ...
-dict_1 = ...
-dict_2 = ...
-dict_3 = ...
-dict_4 = ...
+# Een lijst aanmaken om de dictionaries op te slaan
+personen = []
+
+# Dictionaries aanmaken voor vier personen en toevoegen aan de lijst
+persoon1 = {"id": 1, "voornaam": "John", "achternaam": "Doe"}
+persoon2 = {"id": 2, "voornaam": "Jane", "achternaam": "Smith"}
+persoon3 = {"id": 3, "voornaam": "Alice", "achternaam": "Johnson"}
+persoon4 = {"id": 4, "voornaam": "Bob", "achternaam": "Brown"}
+
+personen.extend([persoon1, persoon2, persoon3, persoon4])
+
+# De volledige naam van de tweede persoon afdrukken
+print(personen[1]["voornaam"], personen[1]["achternaam"])
+
