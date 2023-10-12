87c57d2347946b717d7375146edc932bea5a5f22
Your Name <you@example.com>
2023-10-12 11:29:32 +0200

    opdr_2

--- opdrachten/030_berekeningen/opdr_2/opdr_2.py
+++ opdrachten/030_berekeningen/opdr_2/opdr_2.py
@@ -1,10 +1,26 @@
-# Opdracht 2 berekeningen
-# Naam student:
-# Groep:
+# Definieer de Celsius- en Fahrenheit-variabelen
+c1 = -12
+f1 = 144
 
-# Hier komt je code...
+# Reken Celsius naar Fahrenheit om
+fahrenheit1 = (c1 * 9/5) + 32
 
-c = ...
-f = ...
+# Reken Fahrenheit naar Celsius om
+celsius1 = (f1 - 32) * 5/9
 
-print()
+# Geef de resultaten weer
+print(f"{c1} graden Celsius is gelijk aan {fahrenheit1:.1f} graden Fahrenheit")
+print(f"{f1} graden Fahrenheit is gelijk aan {celsius1:.1f} graden Celsius")
+
+c2 = 62.2
+f2 = 96
+
+# Reken Celsius naar Fahrenheit om
+fahrenheit2 = (c2 * 9/5) + 32
+
+# Reken Fahrenheit naar Celsius om
+celsius2 = (f2 - 32) * 5/9
+
+# Geef de resultaten weer
+print(f"{c2} graden Celsius is gelijk aan {fahrenheit2:.1f} graden Fahrenheit")
+print(f"{f2} graden Fahrenheit is gelijk aan {celsius2:.1f} graden Celsius")
