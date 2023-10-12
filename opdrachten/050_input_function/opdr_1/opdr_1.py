9e3d15c8842ec3457b9af4c75a730f819473fab7
Your Name <you@example.com>
2023-10-12 12:14:19 +0200

    pdracht 5-1

--- opdrachten/050_input_function/opdr_1/opdr_1.py
+++ opdrachten/050_input_function/opdr_1/opdr_1.py
@@ -5,3 +5,14 @@
 # Hier komt je code, maak gebruik van de input functie om de lengte van de rechthoekzijden van de driehoek op te vragen.
 
 
+# Vraag de lengte van de eerste zijde
+zijde1 = float(input("Geef de lengte van de eerste zijde\n"))
+
+# Vraag de lengte van de tweede zijde
+zijde2 = float(input("Geef de lengte van de tweede zijde\n"))
+
+# Bereken de lengte van de schuine zijde met de stelling van Pythagoras
+schuine_zijde = (zijde1**2 + zijde2**2)**0.5
+
+# Druk het resultaat af
+print(f"De lengte van de schuine zijde is: {schuine_zijde}")
--- opdrachten/050_input_function/opdr_2/opdr_2.py
+++ opdrachten/050_input_function/opdr_2/opdr_2.py
@@ -3,5 +3,21 @@
 # Groep:
 
 # Hier komt je code...
+# Maak een lijst van de oorspronkelijke gasten
+gasten = ["Jij", "Paul", "Kees", "Marie", "Hilda"]
 
-gasten = ["Jij", ... ]
+# Print de oorspronkelijke lijst
+print(gasten)
+
+# Verwijder Marie uit de lijst
+gasten.remove("Marie")
+
+# Print de bijgewerkte lijst na het verwijderen van Marie
+print(gasten)
+
+# Voeg George naast Kees toe
+kees_index = gasten.index("Kees")
+gasten.insert(kees_index + 1, "George")
+
+# Print de definitieve lijst met George naast Kees
+print(gasten)
